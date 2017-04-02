2fb5eadbf3166257c8bc49c45dd47677c5b64d1e
lab <lab@Win64-56.ue.loc>
2017-04-02 11:23:24 +0200

    pierwsze webservice

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,4 +12,7 @@ class HomeController extends Controller
     public function index(){
         return view("welcome");
     }
+   
+        
+   
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,7 @@
 |
 */
 Route::get('/', array('as' => 'home', 'uses' => 'HomeController@index'));
-Route::get('doctor/{id?}', array('as' => 'doctor.get', 'uses' => 'DoctorController@show'));
-
+Route::get('doctor', array('as' => 'read.doctors', 'uses' => 'DoctorController@show'));
+Route::get('doctor/{id}', array('as' => 'read.doctor', 'uses' => 'DoctorController@get'));
+Route::get('doctor/{id}/appointment', array('as' => 'read.doctor_appointments', 'uses' => 'DoctorController@getAppointments'));
 
